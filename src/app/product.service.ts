b9bd15cc7d85a378cfb87a908ee2cd44c3cf9c37
Chen <Jiani.Chen@bnymellon.com>
2020-03-01 22:44:32 -0500

    Completed module

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -1,8 +1,12 @@
 import { Injectable } from '@angular/core';
-
+import {Http, Response} from '@angular/http';
 @Injectable()
 export class ProductService {
 
-  constructor() { }
+  constructor(private _http: Http) { }
+  private _albumUrl = '../assets/album.json';
 
+  getAlbum(number id){
+    
+}
 }
